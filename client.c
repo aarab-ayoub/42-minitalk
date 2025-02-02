f63053f94ec270935547cbb44ea90014757a106a
aarab-ayoub <ayoubaarab571@gmail.com>
2025-02-02 13:04:20 +0100

    finish

--- client.c
+++ client.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ayaarab <ayaarab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 17:13:42 by ayaarab           #+#    #+#             */
-/*   Updated: 2025/02/01 23:22:46 by ayoub            ###   ########.fr       */
+/*   Updated: 2025/02/02 13:02:23 by ayaarab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,61 +20,70 @@
 
 void	send_bit(int pid, char bit)
 {
-    if (bit == '0')
-        kill(pid, SIGUSR1);
-    else
-        kill(pid, SIGUSR2);
-    usleep(100);
+	if (bit == '0')
+		kill(pid, SIGUSR1);
+	else
+		kill(pid, SIGUSR2);
+	usleep(100);
+}
+
+void	success(int sig)
+{
+	if (sig == SIGUSR1)
+		ft_printf(GREEN "✅ Message sent successfully!\n" RESET);
+	else
+		ft_printf(RED "❌ Message failed to send!\n" RESET);
+	exit(0);
 }
 
 void	send_message(int pid, char *message)
 {
-    int	i;
-    int	bit;
+	int	i;
+	int	bit;
 
-    i = 0;
-    while (message[i])
-    {
-        bit = 7;
-        while (bit >= 0)
-        {
-            if (message[i] & (1 << bit))
-                send_bit(pid, '1');
-            else
-                send_bit(pid, '0');
-            usleep(500);
-            bit--;
-        }
-        i++;
-    }
-    bit = 7;
-    while (bit >= 0)
-    {
-        send_bit(pid,'0');
-        bit--;
-    }
+	i = 0;
+	while (message[i])
+	{
+		bit = 7;
+		while (bit >= 0)
+		{
+			if (message[i] & (1 << bit))
+				send_bit(pid, '1');
+			else
+				send_bit(pid, '0');
+			usleep(500);
+			bit--;
+		}
+		i++;
+	}
+	bit = 7;
+	while (bit >= 0)
+	{
+		send_bit(pid, '0');
+		bit--;
+	}
 }
 
 int	main(int argc, char **argv)
 {
-    int pid;
+	int	pid;
 
-    if (argc != 3)
-    {
-        printf(RED "❌ Usage: %s <server_pid> <message>\n" RESET, argv[0]);
-        return (1);
-    }
-    else
-    {
-        pid = ft_atoi(argv[1]);
-        if (pid <= 0)
-        {
-            printf(RED "❌ Invalid PID: %s\n" RESET, argv[1]);
-            return (1);
-        }
-        printf(GREEN "✅ Sending message to PID %d...\n" RESET, pid);
-        send_message(pid, argv[2]);
-        printf(GREEN "✅ Message sent successfully! 🎉\n" RESET);
-    }
-    return (0);
+	if (argc != 3)
+	{
+		ft_printf(RED "❌ Usage: %s <server_pid> <message>\n" RESET, argv[0]);
+		return (1);
+	}
+	else
+	{
+		signal(SIGUSR1, success);
+		pid = ft_atoi(argv[1]);
+		if (pid <= 0)
+		{
+			ft_printf(RED "❌ Invalid PID: %s\n" RESET, argv[1]);
+			return (1);
+		}
+		ft_printf(GREEN "✅ Sending message to PID %d...\n" RESET, pid);
+		send_message(pid, argv[2]);
+	}
+	return (0);
 }
--- minitalk.h
+++ minitalk.h
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ayaarab <ayaarab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 16:59:36 by ayaarab           #+#    #+#             */
-/*   Updated: 2025/02/02 00:28:45 by ayoub            ###   ########.fr       */
+/*   Updated: 2025/02/02 12:49:35 by ayaarab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include <unistd.h>
 #include "42-ft_printf/ft_printf.h"
-#include <signal.h>
 #include <limits.h>
+#include <signal.h>
+#include <unistd.h>
 
 int	ft_atoi(char *str);
